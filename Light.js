d9eada38519a4e95f7ecb4fe80c1927e910c62af
kikoxvx <65149479+kikoxvx@users.noreply.github.com>
2023-08-19 16:17:09 +0800

    Create Light.js

---
+++ Light.js
@@ -0,0 +1,27 @@
+const anni = {};
+const anni1 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+
+if (typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  anni.headers = $request.headers;
+} else if (anni1 && anni1.subscriber) {
+  anni1.subscriber.subscriptions = anni1.subscriber.subscriptions || {};
+  anni1.subscriber.entitlements = anni1.subscriber.entitlements || {};
+
+  const data = {
+    "expires_date": "2099-12-31T12:00:00Z",
+    "original_purchase_date": "2022-11-18T03:57:16Z",
+    "purchase_date": "2022-06-15T12:00:00Z",
+    "ownership_type": "PURCHASED",
+    "store": "app_store"
+  };
+
+  anni1.subscriber.subscriptions["com.miiiao.premium"] = data;
+  anni1.subscriber.entitlements["premium_rc"] = JSON.parse(JSON.stringify(data));
+  anni1.subscriber.entitlements["premium_rc"].product_identifier = "com.miiiao.premium";
+
+  anni.body = JSON.stringify(anni1);
+}
+
+$done(anni);
